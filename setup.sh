c46d085749a531d7e89b131ced30951d1bc3ff9c
Mitsuo Heijo <mitsuo_h@outlook.com>
2018-11-26 11:57:33 +0900

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -15,12 +15,12 @@ REPO_DIR=`echo "$1" | sed -e 's|/$||g'`
 TARGET_DIR=`echo "$2" | sed -e 's|/$||g'`
 
 echo "REPO_DIR=$REPO_DIR, TARGET_DIR=$TARGET_DIR"
-echo -n "Are you OK? (y): "
+echo "Are you OK? (y)"
 read yes
 
 if [ $yes = "y" ]; then
   for d in "bash" "vim" "nvim" "scrips" "tmux"; do
-    rm -f $TARGET_DIR/$d
+    rm -rf $TARGET_DIR/$d
     ln -s $REPO_DIR/$d $TARGET_DIR/$d
     echo "Linked $REPO_DIR/$d -> $TARGET_DIR/$d"
   done
