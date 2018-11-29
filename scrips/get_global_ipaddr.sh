b5ac5864f16a23b0450c523e77e986bb2137ac0b
Mitsuo Heijo <mitsuo_h@outlook.com>
2018-11-30 00:24:09 +0900

    fix get_global_ipaddr.sh

--- scrips/get_global_ipaddr.sh
+++ scrips/get_global_ipaddr.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 REMOTE_URL="https://httpbin.org/ip"
+IPADDR_PATTERN="([0-9]+.){3}[0-9]+" # This is not an appropriate regular expression to match an IP address.
 
 if type curl > /dev/null 2>&1; then
-  curl -sSL $REMOTE_URL | awk '{if(match($0,/([0-9]+\.){3}[0-9]+/)) print substr($0, RSTART, RLENGTH)}'
+  curl -sSL $REMOTE_URL | awk -v p=$IPADDR_PATTERN '{if(match($0,p)) print substr($0, RSTART, RLENGTH)}'
 elif type wget > /dev/null 2>&1; then
-  wget -q -O - $REMOTE_URL | awk '{if(match($0,/([0-9]+\.){3}[0-9]+/)) print substr($0, RSTART, RLENGTH)}'
+  wget -q -O - $REMOTE_URL | awk -v p=$IPADDR_PATTERN '{if(match($0,p)) print substr($0, RSTART, RLENGTH)}'
 else
   echo "\'curl\' or \'wget\' not found."
   exit 1
