6d5065c9b2b5dca3332e576597917a2f6ed7cc91
Mitsuo Heijo <mitsuo_h@outlook.com>
2019-01-03 22:22:12 +0900

    remove unnecessary sudo

--- scrips/remove_disabled_snap.sh
+++ scrips/remove_disabled_snap.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if type snap > /dev/null 2>&1; then
-  sudo snap list --all | grep disabled | awk '{print $1,$3}' |
+  snap list --all | grep disabled | awk '{print $1,$3}' |
     while read -r name rev; do
       sudo snap remove "$name" --revision "$rev";
     done
