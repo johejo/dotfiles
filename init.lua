e152bd78c981bf85f115b9b9292ab183fc60daa3
Mitsuo Heijo <mitsuo.heijo@gmail.com>
2022-08-02 14:29:05 +0900

    update

--- init.lua
+++ init.lua
@@ -48,7 +48,6 @@ vim.opt.number = true
 vim.opt.updatetime = 250
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
-vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
 vim.opt.completeopt = "menu,menuone,noselect"
 vim.opt.signcolumn = "yes"
