77ab04207766bad14608183beb3fb56caf9a197a
Mitsuo Heijo <mitsuo.heijo@gmail.com>
2025-02-08 23:29:22 +0900

    update

--- .zshrc
+++ .zshrc
@@ -1,88 +1,61 @@
 bindkey -e
 
-eval "$(starship init zsh)"
-
-alias es="exec $SHELL -l"
-alias ls="exa"
-alias ll="exa -al"
+alias es='exec $SHELL'
+alias ls="eza"
+alias ll="eza -al"
 alias cat="bat"
 alias k="kubectl"
+alias tailscale="/Applications/Tailscale.app/Contents/MacOS/Tailscale"
 
 export FZF_DEFAULT_OPTS="--layout=reverse"
 export EDITOR="nvim"
-export GPG_TTY=$(tty)
-export MANPAGER="nvim -c ASMANPAGER -"
 export HISTSIZE=99999
 export SAVEHIST=999999
+
 setopt hist_ignore_dups
 setopt hist_ignore_all_dups
 setopt hist_reduce_blanks
 
 function gcd {
-  cd "$(ghq root)/$(ghq list | fzf)/"
+	cd "$(ghq root)/$(ghq list | fzf)/" || exit
 }
 
-eval "$($HOME/go/bin/safepathexport -key PATH -value $HOME/go/bin -push)"
+eval "$("$HOME/go/bin/safepathexport" -key PATH -value "$HOME/go/bin" -push)"
 
-eval "$(safepathexport -key PATH -value $HOME/.local/bin -push)"
+eval "$(safepathexport -key PATH -value "$HOME/.local/bin" -push)"
+eval "$(safepathexport -key PATH -value "$HOME/.local/nvim/bin")"
+eval "$(safepathexport -key PATH -value "$HOME/.krew/bin" -push)"
+eval "$(safepathexport -key PATH -value "$HOME/.cargo/bin" -push)"
+#eval "$(safepathexport -key PATH -value "$HOME/ghq/github.com/golang/go/bin")"
 
-eval "$(safepathexport -key PATH -value $HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin)"
-eval "$(safepathexport -key PATH -value $HOMEBREW_PREFIX/opt/gnu-tar/libexec/gnubin)"
-eval "$(safepathexport -key PATH -value $HOMEBREW_PREFIX/opt/gnu-sed/libexec/gnubin)"
-eval "$(safepathexport -key PATH -value $HOMEBREW_PREFIX/opt/gnu-time/libexec/gnubin)"
-eval "$(safepathexport -key PATH -value $HOMEBREW_PREFIX/opt/grep/libexec/gnubin)"
-eval "$(safepathexport -key PATH -value $HOMEBREW_PREFIX/opt/make/libexec/gnubin)"
-eval "$(safepathexport -key PATH -value $HOMEBREW_PREFIX/opt/node@16/bin)"
-eval "$(safepathexport -key PATH -value $HOMEBREW_PREFIX/opt/openjdk/bin)"
+eval "$(safepathexport -key PATH -value "$HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin")"
+eval "$(safepathexport -key PATH -value "$HOMEBREW_PREFIX/opt/gnu-tar/libexec/gnubin")"
+eval "$(safepathexport -key PATH -value "$HOMEBREW_PREFIX/opt/gnu-sed/libexec/gnubin")"
+eval "$(safepathexport -key PATH -value "$HOMEBREW_PREFIX/opt/gnu-time/libexec/gnubin")"
+eval "$(safepathexport -key PATH -value "$HOMEBREW_PREFIX/opt/grep/libexec/gnubin")"
+eval "$(safepathexport -key PATH -value "$HOMEBREW_PREFIX/opt/make/libexec/gnubin")"
 
-eval "$(safepathexport -key MANPATH -value $HOMEBREW_PREFIX/opt/coreutils/libexec/man)"
-eval "$(safepathexport -key MANPATH -value $HOMEBREW_PREFIX/opt/gnu-tar/libexec/man)"
-eval "$(safepathexport -key MANPATH -value $HOMEBREW_PREFIX/opt/gnu-sed/libexec/man)"
-eval "$(safepathexport -key MANPATH -value $HOMEBREW_PREFIX/opt/gnu-time/libexec/man)"
-eval "$(safepathexport -key MANPATH -value $HOMEBREW_PREFIX/opt/grep/libexec/man)"
-eval "$(safepathexport -key MANPATH -value $HOMEBREW_PREFIX/opt/make/libexec/man)"
-eval "$(safepathexport -key MANPATH -value $HOMEBREW_PREFIX/opt/node@16/share/man)"
-eval "$(safepathexport -key MANPATH -value $HOMEBREW_PREFIX/opt/openjdk/share/man)"
+eval "$(safepathexport -key MANPATH -value "$HOMEBREW_PREFIX/opt/coreutils/libexec/man")"
+eval "$(safepathexport -key MANPATH -value "$HOMEBREW_PREFIX/opt/gnu-tar/libexec/man")"
+eval "$(safepathexport -key MANPATH -value "$HOMEBREW_PREFIX/opt/gnu-sed/libexec/man")"
+eval "$(safepathexport -key MANPATH -value "$HOMEBREW_PREFIX/opt/gnu-time/libexec/man")"
+eval "$(safepathexport -key MANPATH -value "$HOMEBREW_PREFIX/opt/grep/libexec/man")"
+eval "$(safepathexport -key MANPATH -value "$HOMEBREW_PREFIX/opt/make/libexec/man")"
 
-### Added by Zinit's installer
-if [[ ! -f $HOME/.local/share/zinit/zinit.git/zinit.zsh ]]; then
-  print -P "%F{33} %F{220}Installing %F{33}ZDHARMA-CONTINUUM%F{220} Initiative Plugin Manager (%F{33}zdharma-continuum/zinit%F{220})…%f"
-  command mkdir -p "$HOME/.local/share/zinit" && command chmod g-rwX "$HOME/.local/share/zinit"
-  command git clone https://github.com/zdharma-continuum/zinit "$HOME/.local/share/zinit/zinit.git" && \
-    print -P "%F{33} %F{34}Installation successful.%f%b" || \
-    print -P "%F{160} The clone has failed.%f%b"
-fi
+eval "$(safepathexport -key FPATH -value "$HOMEBREW_PREFIX/share/zsh/site-functions" -shell)"
+eval "$(safepathexport -key FPATH -value "$HOMEBREW_PREFIX/completions/zsh" -shell)"
 
-source "$HOME/.local/share/zinit/zinit.git/zinit.zsh"
-autoload -Uz _zinit
-(( ${+_comps} )) && _comps[zinit]=_zinit
+export PURE_PROMPT_SYMBOL="$"
+eval "$(sheldon source)"
 
-# Load a few important annexes, without Turbo
-# (this is currently required for annexes)
-zinit light-mode for \
-    zdharma-continuum/zinit-annex-as-monitor \
-    zdharma-continuum/zinit-annex-bin-gem-node \
-    zdharma-continuum/zinit-annex-patch-dl \
-    zdharma-continuum/zinit-annex-rust
-
-### End of Zinit's installer chunk
-
-zinit load zdharma-continuum/history-search-multi-word
-zinit light zdharma-continuum/fast-syntax-highlighting
-zinit light zsh-users/zsh-autosuggestions
-zinit light zsh-users/zsh-syntax-highlighting
-zinit light zsh-users/zsh-history-substring-search
-zinit light zsh-users/zsh-completions
-
-FPATH="$HOMEBREW_PREFIX/share/zsh/site-functions:$FPATH"
-FPATH="$HOMEBREW_PREFIX/completions/zsh:$FPATH"
+autoload -Uz compinit && compinit
+autoload -Uz bashcompinit && bashcompinit
 
 zstyle ':completion:*' verbose yes
 zstyle ':completion:*' completer _oldlist _complete _ignored
 zstyle ':completion:*' use-cache true
 zstyle ':completion:*:default' menu select=1
 
-autoload -Uz compinit && compinit
-autoload -U bashcompinit && bashcompinit
-
-source "$HOMEBREW_PREFIX/share/zsh/site-functions/_google-cloud-sdk"
+export VULTR_API_KEY=GI2HH43CKFAIVDHJRTFIHUGCGR7NMA7AOKAQ
+export GOTOOLCHAIN=local
+export TF_CLI_ARGS_plan="--parallelism=30"
--- init.lua
+++ init.lua
@@ -1,309 +1,312 @@
 vim.cmd("packadd vim-jetpack")
 
-require("jetpack.paq")({
-  { "tani/vim-jetpack", opt = 1 },
-  "neovim/nvim-lspconfig",
-  "b0o/schemastore.nvim",
-  "nvim-lua/plenary.nvim",
-  "jose-elias-alvarez/null-ls.nvim",
-  "antoinemadec/FixCursorHold.nvim",
-  "lukas-reineke/indent-blankline.nvim",
-  "hrsh7th/nvim-pasta",
-  "hrsh7th/cmp-nvim-lsp",
-  "hrsh7th/cmp-buffer",
-  "hrsh7th/cmp-path",
-  "hrsh7th/cmp-emoji",
-  "hrsh7th/cmp-cmdline",
-  "hrsh7th/cmp-vsnip",
-  "hrsh7th/cmp-calc",
-  "hrsh7th/vim-vsnip",
-  "hrsh7th/cmp-nvim-lsp-signature-help",
-  "hrsh7th/cmp-nvim-lsp-document-symbol",
-  "octaltree/cmp-look",
-  "ray-x/cmp-treesitter",
-  "dmitmel/cmp-cmdline-history",
-  "meetcw/cmp-browser-source",
-  "hrsh7th/nvim-cmp",
-  "nvim-treesitter/nvim-treesitter",
-  "kana/vim-submode",
-  "vijaymarupudi/nvim-fzf",
-  "ibhagwan/fzf-lua",
-  "kyazdani42/nvim-web-devicons",
-  "lambdalisue/fern.vim",
-  "lambdalisue/fern-git-status.vim",
-  "lambdalisue/fern-hijack.vim",
-  "lambdalisue/nerdfont.vim",
-  "lambdalisue/fern-renderer-nerdfont.vim",
-  "lambdalisue/glyph-palette.vim",
-  "zdharma-continuum/zinit-vim-syntax",
-  "srcery-colors/srcery-vim",
-  { "iamcco/markdown-preview.nvim", ft = "markdown", run = "cd app && yarn install" },
-  { "kevinhwang91/nvim-bqf", ft = "qf" },
-  "junegunn/fzf",
-  "windwp/nvim-autopairs",
-  "yioneko/nvim-yati",
-  "monaqa/dial.nvim",
-  "yuki-yano/fern-preview.vim",
-  "lambdalisue/vim-manpager",
-  "google/vim-jsonnet",
-  "EdenEast/nightfox.nvim",
-  "rebelot/kanagawa.nvim",
-  "bluz71/vim-moonfly-colors",
-  "rmehri01/onenord.nvim",
+require("jetpack.packer").add({
+	{ "tani/vim-jetpack" },
+	{ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" },
+	{ "neovim/nvim-lspconfig" },
+	{ "stevearc/dressing.nvim" },
+	{ "nvim-tree/nvim-web-devicons" },
+	{ "ibhagwan/fzf-lua" },
+	{ "hrsh7th/nvim-cmp" },
+	{ "hrsh7th/cmp-nvim-lsp" },
+	{ "hrsh7th/cmp-nvim-lsp-signature-help" },
+	{ "hrsh7th/cmp-buffer" },
+	{ "hrsh7th/cmp-path" },
+	{ "hrsh7th/cmp-vsnip" },
+	{ "hrsh7th/vim-vsnip" },
+	{ "octaltree/cmp-look" },
+	{ "b0o/schemastore.nvim" },
+	{ "windwp/nvim-autopairs" },
+	--	{ "zbirenbaum/copilot.lua" },
+	--	{ "zbirenbaum/copilot-cmp" },
+	{ "projekt0n/github-nvim-theme" },
+	{ "sainnhe/gruvbox-material" },
+	{ "lukas-reineke/cmp-rg" },
+	{ "bydlw98/cmp-env" },
+	{ "lambdalisue/fern.vim" },
+	{ "lambdalisue/fern-git-status.vim" },
+	{ "lambdalisue/fern-hijack.vim" },
+	{ "lambdalisue/nerdfont.vim" },
+	{ "lambdalisue/fern-renderer-nerdfont.vim" },
+	{ "lambdalisue/glyph-palette.vim" },
 })
 
-vim.cmd("colorscheme srcery")
+vim.g.everforest_background = "soft"
+vim.g.gruvbox_material_background = "hard"
+vim.g.gruvbox_material_foreground = "hard"
+vim.cmd.colorscheme("gruvbox-material")
 
-vim.opt.swapfile = false
-vim.opt.number = true
-vim.opt.updatetime = 250
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.completeopt = "menu,menuone,noselect"
-vim.opt.signcolumn = "yes"
-vim.opt.clipboard = "unnamedplus"
-vim.opt.termguicolors = true
-vim.opt.cursorline = true
-vim.opt.hlsearch = false
-vim.opt.ignorecase = true
-vim.opt.list = true
-vim.opt.listchars = "tab:»-,trail:-,eol:↲,extends:»,precedes:«,nbsp:%"
-vim.opt.wrap = false
+--require("copilot").setup({
+--	suggestion = { enabled = false },
+--	panel = { enabled = false },
+--})
 
-require("nvim-treesitter.configs").setup({
-  ensure_installed = {
-    "c",
-    "cpp",
-    "lua",
-    "go",
-    "gomod",
-    "dockerfile",
-    "bash",
-    "cmake",
-    "comment",
-    "vim",
-    "hcl",
-    "html",
-    "json",
-    "make",
-    "php",
-    "toml",
-    "cmake",
-    "jsdoc",
-    "proto",
-    "markdown",
-    "regex",
-    "toml",
-    "tsx",
-    "typescript",
-    "javascript",
-    "jsdoc",
-    "yaml",
-    "python",
-    "zig",
-    "rust",
-    "sql",
-    "make",
-    "jsonnet",
-  },
-  highlight = {
-    enable = true,
-  },
-  indent = {
-    enable = false,
-  },
-  yati = {
-    enable = true,
-  },
-})
+--require("copilot_cmp").setup()
 
-require("cmp-browser-source").start_server()
-
-local cmp = require("cmp")
-cmp.setup({
-  snippet = {
-    expand = function(args)
-      vim.fn["vsnip#anonymous"](args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ["<CR>"] = cmp.mapping.confirm({ select = true }),
-    ["<Tab>"] = cmp.mapping.confirm({ select = true }),
-  }),
-  sources = cmp.config.sources({
-    { name = "nvim_lsp" },
-    { name = "buffer" },
-    { name = "path" },
-    { name = "emoji" },
-    { name = "treesitter" },
-    { name = "vsnip" },
-    { name = "calc" },
-    { name = "nvim_lsp_signature_help" },
-    { name = "browser" },
-    { name = "look", keyword_length = 2, convert_case = true, loud = true },
-  }),
-})
-cmp.setup.cmdline("/", {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({
-    { name = "buffer" },
-    { name = "nvim_lsp_document_symbol" },
-  }),
-})
-cmp.setup.cmdline(":", {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({
-    { name = "path" },
-    { name = "cmdline" },
-    { name = "cmdline_history" },
-  }),
-})
-
-local schemastore = require("schemastore")
-
-local null_ls = require("null-ls")
-null_ls.setup({
-  sources = {
-    null_ls.builtins.formatting.prettier.with({
-      only_local = "node_modules/.bin",
-    }),
-    null_ls.builtins.diagnostics.shellcheck,
-    null_ls.builtins.formatting.stylua,
-    null_ls.builtins.formatting.shfmt,
-    null_ls.builtins.formatting.clang_format,
-    null_ls.builtins.diagnostics.actionlint,
-    null_ls.builtins.diagnostics.checkmake,
-    null_ls.builtins.diagnostics.cppcheck,
-    -- null_ls.builtins.formatting.deno_fmt,
-  },
-  on_attach = function()
-    vim.keymap.set("n", "<C-f>", function()
-      vim.lsp.buf.format({ timeout_ms = 10000 })
-    end, { buffer = true })
-  end,
-})
-
-local lspconfig = require("lspconfig")
-
-local language_servers = {
-  gopls = {
-    formatting_fn = function()
-      vim.lsp.buf.format({ async = false })
-      local params = vim.lsp.util.make_range_params()
-      params.context = { only = { "source.organizeImports" } }
-      local results = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
-      for _, res in pairs(results or {}) do
-        for _, r in pairs(res.result or {}) do
-          if r.edit then
-            vim.lsp.util.apply_workspace_edit(r.edit, "utf-16")
-          elseif r.command then
-            vim.lsp.buf.execute_command(r.command)
-          end
-        end
-      end
-    end,
-  },
-  tsserver = {
-    disable_format = true,
-    root_dir = lspconfig.util.root_pattern("package.json"),
-  },
-  eslint = {},
-  jsonls = {
-    settings = {
-      json = {
-        schemas = schemastore.json.schemas(),
-      },
-    },
-  },
-  cssls = {},
-  vimls = {},
-  yamlls = {},
-  pyright = {},
-  sumneko_lua = {
-    settings = {
-      Lua = {
-        runtime = {
-          version = "LuaJIT",
-        },
-        diagnostics = {
-          globals = { "vim" },
-        },
-        workspace = {
-          library = vim.api.nvim_get_runtime_file("", true),
-        },
-        telemetry = {
-          enable = false,
-        },
-      },
-    },
-    disable_format = true,
-  },
-  clangd = {
-    disable_format = true,
-    capabilities_fn = function(capabilities)
-      capabilities.offsetEncoding = { "utf-16" }
-      return capabilities
-    end,
-  },
-  terraformls = {},
-  bashls = {},
-  dockerls = {},
-  marksman = {},
-  zls = {},
-  rust_analyzer = {},
-  denols = {
-    root_dir = lspconfig.util.root_pattern("deno.json"),
-  },
-  jsonnet_ls = {
-    cmd = { "jsonnet-language-server", "--tanka", "--lint", "--eval-diags" },
-  },
-}
-
-local cmp_capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
-cmp_capabilities.textDocument.completion.completionItem.snippetSupport = true
-
-for server, opts in pairs(language_servers) do
-  local capabilities = opts.capabilities_fn and opts.capabilities_fn(cmp_capabilities) or cmp_capabilities
-  require("lspconfig")[server].setup({
-    root_dir = opts.root_dir,
-    capabilities = capabilities,
-    settings = opts.settings,
-    cmd = opts.cmd,
-    on_attach = function(client)
-      local map_opts = { buffer = true }
-      vim.keymap.set("n", "H", vim.lsp.buf.hover, map_opts)
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, map_opts)
-      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, map_opts)
-      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, map_opts)
-      vim.keymap.set("n", "<C-q>", vim.lsp.buf.references, map_opts)
-      vim.keymap.set("n", "<Space>rn", vim.lsp.buf.rename, map_opts)
-      if opts.disable_format then
-        client.server_capabilities.documentFormattingProvider = false
-        client.server_capabilities.documentRangeFormattingProvider = false
-      else
-        if opts.formatting_fn then
-          vim.keymap.set("n", "<C-f>", opts.formatting_fn, map_opts)
-        else
-          vim.keymap.set("n", "<C-f>", vim.lsp.buf.format, map_opts)
-        end
-      end
-    end,
-  })
-end
-
-vim.keymap.set("n", "<C-d>", vim.diagnostic.open_float)
-vim.diagnostic.config({ virtual_text = false, update_in_insert = true })
-
-require("indent_blankline").setup({
-  show_end_of_line = true,
-  show_current_context = true,
+local autopairs = require("nvim-autopairs")
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+autopairs.setup({
+	check_ts = true,
 })
 
 local fzf_lua = require("fzf-lua")
 vim.keymap.set("n", "..", fzf_lua.files)
-vim.keymap.set("n", "<Space>g", fzf_lua.live_grep)
+vim.keymap.set("n", "<Space>g", fzf_lua.live_grep_resume)
 vim.keymap.set("n", "<C-p>", fzf_lua.oldfiles)
+vim.keymap.set("n", "<C-b>", fzf_lua.buffers)
+
+local cmp = require("cmp")
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+cmp.setup({
+	formatting = {
+		format = function(entry, vim_item)
+			vim_item.menu = ({
+				buffer = "[buffer]",
+				nvim_lsp = "[LSP]",
+				vsnip = "[vsnip]",
+				path = "[path]",
+				look = "[look]",
+				--copilot = "[copilot]",
+				rg = "[rg]",
+				env = "[env]",
+			})[entry.source.name]
+			return vim_item
+		end,
+	},
+	snippet = {
+		expand = function(args)
+			vim.fn["vsnip#anonymous"](args.body)
+		end,
+	},
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" },
+		{ name = "nvim_lsp_signature_help" },
+		{ name = "vsnip" },
+		{ name = "buffer" },
+		{ name = "path" },
+		{ name = "look" },
+		--		{ name = "copilot" },
+		{ name = "rg" },
+		{ name = "env" },
+	}),
+	mapping = cmp.mapping.preset.insert({
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
+		["<Tab>"] = cmp.mapping.confirm({ select = true }),
+	}),
+})
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+require("nvim-treesitter.configs").setup({
+	ensure_installed = {
+		"lua",
+		"go",
+		"typescript",
+		"vim",
+		"json",
+		"dockerfile",
+		"terraform",
+	},
+	highlight = {
+		enable = true,
+	},
+	indent = {
+		enable = true,
+	},
+})
+
+local lspconfig = require("lspconfig")
+local runtime_path = vim.split(package.path, ";")
+lspconfig.gopls.setup({
+	capabilities = capabilities,
+	settings = {
+		gopls = {
+			staticcheck = true,
+		},
+	},
+})
+lspconfig.vtsls.setup({
+	capabilities = capabilities,
+	init_options = { documentFormatting = false },
+})
+lspconfig.eslint.setup({
+	capabilities = capabilities,
+})
+lspconfig.jsonls.setup({
+	settings = {
+		json = {
+			schemas = require("schemastore").json.schemas(),
+			validate = { enable = true },
+		},
+	},
+})
+lspconfig.yamlls.setup({
+	capabilities = capabilities,
+	settings = {
+		yaml = {
+			schemaStore = {
+				enable = false,
+				url = "",
+			},
+			schemas = require("schemastore").yaml.schemas(),
+		},
+	},
+})
+lspconfig.dockerls.setup({
+	capabilities = capabilities,
+})
+lspconfig.terraformls.setup({
+	capabilities = capabilities,
+})
+
+local prettier = {
+	formatCommand = "npx --no -- prettier --stdin --stdin-filepath ${INPUT} ${--range-start:charStart} ${--range-end:charEnd}",
+	formatStdin = true,
+	formatCanRange = true,
+	rootMarkers = {
+		".prettierrc",
+		".prettierrc.json",
+		".prettierrc.js",
+		".prettierrc.yml",
+		".prettierrc.yaml",
+		".prettierrc.json5",
+		".prettierrc.mjs",
+		".prettierrc.cjs",
+		".prettierrc.toml",
+	},
+}
+local stylua = {
+	formatCommand = "stylua ${--indent-width:tabSize} ${--range-start:charStart} ${--range-end:charEnd} --color Never -",
+	formatStdin = true,
+	formatCanRange = true,
+	rootMarkers = { "stylua.toml", ".stylua.toml" },
+}
+local shfmt = {
+	formatCommand = "shfmt -filename ${INPUT} -",
+	formatStdin = true,
+}
+local shellcheck = {
+	lintCommand = "shellcheck --color=never --format=gcc -",
+	lintStdin = true,
+	lintFormats = { "-:%l:%c: %trror: %m", "-:%l:%c: %tarning: %m", "-:%l:%c: %tote: %m" },
+	rootMarkers = { ".shellcheckrc" },
+}
+local yamlfmt = {
+	formatCommand = "yamlfmt -in",
+	formatStdin = true,
+}
+local hadolint = {
+	lintCommand = "hadolint --no-color -",
+	lintStdin = true,
+	lintFormats = { "-:%l %.%# %trror: %m", "-:%l %.%# %tarning: %m", "-:%l %.%# %tnfo: %m" },
+	rootMarkers = { ".hadolint.yaml" },
+}
+lspconfig.efm.setup({
+	init_options = { documentFormatting = true },
+	filetypes = {
+		"typescript",
+		"typescriptreact",
+		"lua",
+		"zsh",
+		"sh",
+		"bash",
+		"yaml",
+		"dockerfile",
+	},
+	capabilities = capabilities,
+	settings = {
+		languages = {
+			typescript = { prettier },
+			typescriptreact = { prettier },
+			lua = { stylua },
+			zsh = { shfmt, shellcheck },
+			sh = { shfmt, shellcheck },
+			bash = { shfmt, shellcheck },
+			yaml = { yamlfmt },
+			dockerfile = { hadolint },
+		},
+	},
+})
+lspconfig.lua_ls.setup({
+	capabilities = capabilities,
+	settings = {
+		Lua = {
+			format = { enable = false },
+			telemetry = {
+				enable = false,
+			},
+			runtime = {
+				version = "LuaJIT",
+				path = runtime_path,
+			},
+			diagnostic = {
+				globals = { "vim" },
+			},
+			workspace = {
+				library = {
+					vim.env.VIMRUNTIME,
+					vim.api.nvim_get_runtime_file("", true),
+				},
+			},
+		},
+	},
+})
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+	callback = function(ev)
+		local opts = { buffer = ev.buf }
+		vim.keymap.set("n", "<C-f>", vim.lsp.buf.format, opts)
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+		vim.keymap.set("n", "H", vim.lsp.buf.hover, opts)
+		vim.keymap.set("n", "<Space>rn", vim.lsp.buf.rename, opts)
+		vim.keymap.set("n", "<C-i>", function()
+			vim.lsp.buf.code_action({
+				apply = true,
+				context = {
+					only = { "source.organizeImports" },
+				},
+			})
+		end, opts)
+		vim.keymap.set("n", "<C-q>", fzf_lua.lsp_references, opts)
+	end,
+})
+
+vim.api.nvim_create_autocmd({ "FileType", "BufEnter" }, {
+	group = vim.api.nvim_create_augroup("UserGoConfig", {}),
+	pattern = { "go", "caddyfile", "lua" },
+	callback = function()
+		vim.opt_local.expandtab = false
+	end,
+})
+
+vim.api.nvim_create_autocmd({ "FileType", "BufEnter" }, {
+	group = vim.api.nvim_create_augroup("UserGHAConfig", {}),
+	pattern = { "*/.github/workflows/*.y{a,}ml" },
+	callback = function()
+		vim.cmd([[syn region GhaDollarSyntax matchgroup=PreProc start="${{" end="}}" containedin=yamlPlainScalar]])
+	end,
+})
+
+vim.opt.updatetime = 250
+vim.opt.number = true
+vim.opt.swapfile = false
+vim.opt.hlsearch = false
+vim.opt.wrap = false
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.list = true
+vim.opt.termguicolors = true
+vim.opt.clipboard = "unnamedplus"
+vim.opt.signcolumn = "yes"
+vim.opt.listchars = {
+	tab = "▸ ",
+	trail = "·",
+	nbsp = "␣",
+	extends = ">",
+	precedes = "<",
+}
 
 vim.keymap.set({ "n", "v" }, "<C-e>", "5<C-e>")
 vim.keymap.set({ "n", "v" }, "<C-y>", "5<C-y>")
@@ -313,69 +316,22 @@ vim.keymap.set({ "n", "v" }, "<C-k>", "5k")
 vim.keymap.set({ "n", "v" }, "<C-l>", "$")
 vim.keymap.set({ "n", "v" }, "j", "gj")
 vim.keymap.set({ "n", "v" }, "k", "gk")
-vim.cmd([[
-	call submode#enter_with('winsize', 'n', '', '<C-w><C-l>', '<C-w>>')
-	call submode#enter_with('winsize', 'n', '', '<C-w><C-h>', '<C-w><')
-	call submode#enter_with('winsize', 'n', '', '<C-w><C-k>', '<C-w>+')
-	call submode#enter_with('winsize', 'n', '', '<C-w><C-j>', '<C-w>-')
-	call submode#map('winsize', 'n', '', '<C-l>', '<C-w>>')
-	call submode#map('winsize', 'n', '', '<C-h>', '<C-w><')
-	call submode#map('winsize', 'n', '', '<C-k>', '<C-w>+')
-	call submode#map('winsize', 'n', '', '<C-j>', '<C-w>-')
-	let g:fern#renderer = "nerdfont"
 
-	augroup my-glyph-palette
-	  autocmd! *
-	  autocmd FileType fern call glyph_palette#apply()
-	  autocmd FileType nerdtree,startify call glyph_palette#apply()
-	augroup END
-
-	highlight Visual guibg=#839496 gui=None guifg=#002b36
-
-	function! s:fern_settings() abort
-	  nmap <silent> <buffer> p     <Plug>(fern-action-preview:toggle)
-	  nmap <silent> <buffer> <C-p> <Plug>(fern-action-preview:auto:toggle)
-	  nmap <silent> <buffer> <C-d> <Plug>(fern-action-preview:scroll:down:half)
-	  nmap <silent> <buffer> <C-u> <Plug>(fern-action-preview:scroll:up:half)
-	endfunction
-
-	augroup fern-settings
-	  autocmd!
-	  autocmd FileType fern call s:fern_settings()
-	augroup END
-]])
-
-require("pasta").setup({
-  converters = {},
+vim.keymap.set("n", "<C-d>", vim.diagnostic.open_float)
+vim.diagnostic.config({
+	update_in_insert = true,
+	virtual_text = false,
 })
-vim.keymap.set({ "n", "x" }, "p", require("pasta.mappings").p)
-vim.keymap.set({ "n", "x" }, "P", require("pasta.mappings").P)
 
-vim.keymap.set("n", "<Space>f", "<cmd>Fern . -keep -toggle -drawer<CR>")
+require("dressing").setup({})
 
-require("nvim-autopairs").setup()
+vim.g["fern#renderer"] = "nerdfont"
 
-local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-
-local dial_augend = require("dial.augend")
-local dial_config = require("dial.config")
-dial_config.augends:register_group({
-  default = {
-    dial_augend.integer.alias.decimal, -- nonnegative decimal number (0, 1, 2, 3, ...)
-    dial_augend.integer.alias.hex, -- nonnegative hex number  (0x01, 0x1a1f, etc.)
-    dial_augend.date.alias["%Y/%m/%d"], -- date (2022/02/19, etc.)
-    dial_augend.date.alias["%Y-%m-%d"], -- date (2022-02-19, etc.)
-    dial_augend.date.alias["%H:%M:%S"], -- date (12:45:06, etc.)
-    dial_augend.date.alias["%H:%M"], -- date (12:45, etc.)
-    dial_augend.constant.alias.bool,
-    dial_augend.constant.alias.alpha,
-    dial_augend.constant.alias.Alpha,
-    dial_augend.semver.alias.semver,
-  },
+vim.api.nvim_create_autocmd("FileType", {
+	group = vim.api.nvim_create_augroup("UserFernConfig", {}),
+	pattern = { "fern" },
+	callback = function()
+		vim.fn["glyph_palette#apply"]()
+		vim.cmd([[highlight Visual guibg=#839496 gui=None guifg=#002b36]])
+	end,
 })
-local dial_map = require("dial.map")
-vim.keymap.set("n", "<C-a>", dial_map.inc_normal())
-vim.keymap.set("v", "<C-a>", dial_map.inc_visual())
-vim.keymap.set("n", "<C-x>", dial_map.dec_normal())
-vim.keymap.set("v", "<C-x>", dial_map.dec_visual())
